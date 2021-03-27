de542341ee752d3a0cefdb6584e1dbb847c1b25f
user.email <yulki@DESKTOP-22TJNGP>
2021-03-27 22:02:37 +0900

    update 2021-03-27

--- workspace/01_example/src/quiz01/HelloJavaMainClass.java
+++ workspace/01_example/src/quiz01/HelloJavaMainClass.java
@@ -1,7 +1,15 @@
 package quiz01;
 
+import lombok.Data;
+
+@Data
 public class HelloJavaMainClass {
 
+	private int age;
+	private double avg;
+	
+	
+	
 	public static void main(String[] args) {
 
 		System.out.println("Hello Java!");
